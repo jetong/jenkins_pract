98e6b5ca4650b0df974c9c7547bd7dad06efd1d3
jetong <jose.e.tong@gmail.com>
2017-10-03 14:01:04 -0700

    testing with user interaction

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.print("Name: ");
+        Scanner s = new Scanner(System.in);
+        String name = s.next();
+        System.out.println("Hi " + name + "!");
     }
 }
