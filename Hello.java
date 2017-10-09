85236d5a452247a84aba3f18d444cc74d10a19a8
jetong <jose.e.tong@gmail.com>
2017-10-09 10:47:48 -0700

    added exception for Jenkins to catch

--- Hello.java
+++ Hello.java
@@ -1,5 +1,14 @@
 public class Hello {
-    public static void main(String[] args){
-        System.out.println("Hello World!"):;  // error for Jenkins to catch
+  public static void main(String[] args){
+    int a = 1;
+    int b = 0;
+
+    System.out.println("Hello World!");
+    try{
+      double result = a/b;
+      System.out.println("a/b = " + result);
+    }catch(ArithmeticException e){
+      System.out.println("Exception caught: " + e.getMessage());
     }
+  }
 }
